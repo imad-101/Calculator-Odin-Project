a5a646f2612b71d57f9e4776badabde0ded40087
imad-101 <imaduddin10.1973@gmail.com>
2024-07-25 23:49:34 +0500

    Added a handleNumber function to display number on screen

--- scripts.js
+++ scripts.js
@@ -1,45 +1,76 @@
-const screen = document.querySelector('.screen');
+const currentScreen = document.querySelector('.current');
+const previousScreen = document.querySelector('.previous');
+const oppScreen = document.querySelector('.oppscreen')
 const numbersBtn = document.querySelectorAll('.num');
 const delBtn = document.querySelector('#delete');
 const clearBtn = document.querySelector('#clear');
 const operatorBtn = document.querySelectorAll(".operators");
 const equalBtn = document.querySelector('#equal');
 const decimleBtn = document.querySelector('#decimle');
-let operator = "";
-let number = '';
-numbersBtn.forEach((button => {
-    button.addEventListener('click', (e) =>{
+
+numbersBtn.forEach((number) => {
+    number.addEventListener('click' , (e)=>{
         handleNumber(e.target.textContent)
     })
-}))
-operatorBtn.forEach((operator => {
-    operator.addEventListener('click', (e) =>{
-        handleOperator(e.target.textContent)
-    })
-}))
-equalBtn.addEventListener('click', function(){
-    calculate();
 })
+operatorBtn.forEach((opp) => {
+    opp.addEventListener('click' , (e)=>{
+        handleOperator(e.target.textContent)
+    })   
+})
+equalBtn.addEventListener('click', ()=> {
+    operate();
+})
+clearBtn.addEventListener('click', ()=>{
+    currentScreen.textContent = "";
+    previousScreen.textContent = "";
+    oppScreen.textContent = "";
+})
+decimleBtn.addEventListener('click' , ()=>{
+    if(!currentScreen.textContent.includes(".")){
+        currentScreen.textContent += "."
+    }
+})
+delBtn.addEventListener('click' , ()=>{
+    valueToSlice = currentScreen.textContent;
+    valueToSlice = valueToSlice.slice(0, -1);
+    currentScreen.textContent = valueToSlice;
+})
+function handleNumber(number){
+    if(currentScreen.textContent.length <= 6){
+        currentScreen.textContent += number;
+    }
 
+}
 function handleOperator(opp){
-    screen.textContent += opp;
-    operator = opp;
+    oppScreen.textContent = opp;
+    previousScreen.textContent = currentScreen.textContent;
+    currentScreen.textContent = "";
     
 }
-function handleNumber(num){
-    screen.textContent += num;
-    number = num;
+function operate(){
+    operator = oppScreen.textContent;
+    previousValue = Number(previousScreen.textContent);
+    currentValue = Number(currentScreen.textContent);
+    let result = "";
+
+    if(operator == "+"){
+        result = previousValue += currentValue;
+    }
+    else if(operator == "-"){
+        result = previousValue -= currentValue;
+    }
+    else if(operator == "*"){
+        result = previousValue *= currentValue;
+    }
+    else if(operator == "/"){
+        result = previousValue /= currentValue;
+    }
+    currentScreen.textContent = result;
+    previousScreen.textContent = "";
+    oppScreen.textContent = "";
+    result = roundNumber(result);
 }
-function calculate(){
-    number = parseFloat(number);
-    if (operator == "+"){
-        number += number
-        console.log(number)
-    }
-    else if (operator == "/"){
-        number /= number
-        console.log(number)
-    }
-
-
+function roundNumber(num){
+    Math.round((num * 1000)/1000);
 }
